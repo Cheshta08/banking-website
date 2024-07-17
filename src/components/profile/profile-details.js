8aadb4193cc67ad92395d5e61dbb85a6844fc473
Cheshta08 <111150320+Cheshta08@users.noreply.github.com>
2024-07-17 12:30:40 +0530

    Add files via upload

---
+++ src/components/profile/profile-details.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import Profile from './profile';
+import EditModal from './edit';
+import { useParams } from 'react-router-dom';
+
+function ProfileDetails() {
+    const { userId}  = useParams();
+    const [userInfos, setUserInfos] = useState(null);
+    const [modalState, setModalState] = useState({ isOpen: false, section: null });
+    
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/info/${userId}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch user information');
+                }
+                const data = await response.json();
+                setUserInfos(data);
+            } catch (error) {
+                console.error('Error fetching user information:', error);
+            }
+        };
+
+        fetchUserInfo();
+    }, [userId]); // Dependency array ensures useEffect runs on userId change
+
+    const handleEdit = async (section, updatedInfo) => {
+        
+         setModalState({ isOpen: false, section: null });
+    
+        try {
+            const response = await fetch(`http://localhost:5000/update/${userInfos._id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({[section]:updatedInfo}),
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+                setUserInfos(data);
+            
+    
+            console.log("Task Updated");
+        } catch (error) {
+            console.log('Fetch error: ', error);
+        }
+    };
+    
+
+    const openModal = (section) => {
+        setModalState({ isOpen: true, section:section });
+        
+    };
+
+    const closeModal = () => {
+        setModalState({ isOpen: false, section: null });
+    };
+
+    if (!userInfos) {
+        return <div>Loading...</div>; // Handle loading state
+    }
+
+    return (
+        <>
+        {userInfos && userInfos.personal&&
+            <Profile userInfos={userInfos} openModal={openModal} />}
+
+            {modalState.isOpen && modalState.section!=null&& (
+                <EditModal
+                    section={modalState.section}
+                    userInfos={userInfos}
+                    handleEdit={handleEdit}
+                    closeModal={closeModal}
+                />
+            )}
+        </>
+    );
+}
+
+export default ProfileDetails;
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,1 +1,60 @@
+import React from 'react';
+import InfosSection from './infoSection.js';
 
+function Profile({ userInfos, openModal }) {
+    console.log(userInfos.personal);
+
+    if (!userInfos) {
+        return <div>Loading...</div>;
+    }
+    
+    return (
+        <>
+
+            <div className="flex flex-col mr-auto ml-auto w-[900px]">
+                <h1 className="font-[600] text-[30px] mb-[10px] mt-[15px]">Personal Details</h1>
+                <div className="flex flex-row justify-center">
+                    <div className="flex flex-col mr-[20px]">
+                        <InfosSection
+                            title="Personal Information"
+                            info={userInfos.personal}
+                            fields={['firstName', 'middleName', 'lastName', 'dob']}
+                            onEdit={() => openModal('personal')}
+                        />
+                        <InfosSection
+                            title="Contact Details"
+                            info={userInfos.contact}
+                            fields={['email', 'phoneNumber']}
+                            onEdit={() => openModal('contact')}
+                        />
+                    </div>
+                    <div className="flex flex-col mr-[20px]">
+                        <InfosSection
+                            title="Home Address"
+                            info={userInfos.address}
+                            fields={['premise', 'thoroughfare', 'locality', 'postalCode', 'livedDuration']}
+                            onEdit={() => openModal('address')}
+                        />
+                        <InfosSection
+                            title="Nationality & Tax Residency"
+                            info={userInfos.nationality}
+                            fields={['citizenship', 'tax_residence']}
+                            onEdit={() => openModal('nationality')}
+                        />
+
+                        <InfosSection
+                            title="Employment Details"
+                            info={userInfos.employment_details}
+                            fields={['status', 'industry', 'occupation']}
+                            onEdit={() => openModal('employment_details')}
+                        />
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Profile;
